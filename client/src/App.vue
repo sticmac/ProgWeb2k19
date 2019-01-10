c1a473a3deb2bb9b2c22eee703adfa452afeef4a
Antoine Dezarnaud <dezartoine@hotmail.fr>
2019-01-10 11:12:50 +0300

    Basic UI - Buefy + sass

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Accueil/>
+  </div>
+</template>
+
+<script>
+import Accueil from './components/Accueil.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Accueil
+  }
+}
+</script>
+
+<style lang="scss">
+@import './src/styles/global.scss';
+</style>
