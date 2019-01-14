b1011a9232b592e2f454d4532311a722badd17b4
Marc Jourdes <marcjourdes@gmail.com>
2019-01-14 16:05:51 +0100

    Added router and home page

---
+++ client/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <b-tabs size="is-medium" class="block" v-model="selected">
+      <b-tab-item label="Produits" icon="shopping-basket"></b-tab-item>
+      <b-tab-item label="Recettes" icon="list-ul"></b-tab-item>
+    </b-tabs>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      selected: 0
+    };
+  },
+  watch: {
+    selected(value) {
+      if (value === 0) {
+          this.$router.push('home')
+      } else if (value === 1) {
+          this.$router.push('recipes')
+      }
+    }
+  }
+};
+</script>
