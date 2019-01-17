dbd1c5420babf3301b3107ef0ad3c5bf3ce09a63
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-01-17 10:47:32 +0100

    Designing the SearchResult component with Bulma css

--- client/src/components/SearchResult.vue
+++ client/src/components/SearchResult.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="row">
-        <img v-bind:src="meal.thumbUrl" v-bind:alt="meal.name" class="thumbnail">
-        <div class="info">
-          <p class="meal-title">{{meal.name}}</p>
-          <p class="description">{{meal.description}}</p>
+    <div class="card is-secondary">
+        <header class="card-header">
+          <p class="card-header-title">
+            {{meal.name}}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-64x64 is-square">
+                <img v-bind:src="meal.thumbUrl" v-bind:alt="meal.name" class="thumbnail">
+              </figure>
+            </div>
+
+            <div class="media-content">
+              <div class="description">{{meal.description}}</div>
+            </div>
+          </div>
+
         </div>
     </div>
 </template>
@@ -16,40 +30,11 @@ export default {
 
 
 <style>
-.row {
-  display: flex;
-  align-items: center;
-  border: #ccc 1px dashed;
-  margin: auto;
-  height: 125px;
-  transition: background-color 0.2s linear;
-}
-
-.row:hover {
-  background-color: #eee;
-}
-
 .thumbnail {
   object-fit: cover;
-  width: 100px;
-  height: 100px;
-  border: #ccc 1px solid;
-  margin-left: 1em;
-}
-
-.info {
-  flex: 1;
-  align-items: flex-start;
-  height: 80%;
-  margin-left: 1em;
-}
-
-.meal-title {
-  font-weight: bold;
 }
 
 .description {
-  margin: 0.3em;
   overflow: hidden;
   text-overflow: ellipsis;
   /* use this value to count block height */
