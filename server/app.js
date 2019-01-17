345266f36ecf497bb2d4baa4bc42393a6158d4e3
Alexandre HILTCHER <alex.hiltcher@gmail.com>
2019-01-17 10:28:40 +0100

    Setup front serve by node
    Created a build script
    Set default route to serve the app

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -21,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 app.use((req, res, next) => {
