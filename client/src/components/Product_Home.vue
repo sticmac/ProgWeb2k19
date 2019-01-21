f777c076e9d6c2177a82b20dfb79a09224033c64
Unknown <marcjourdes@gmail.com>
2019-01-21 14:44:21 +0100

    Search view

--- client/src/components/Product_Home.vue
+++ client/src/components/Product_Home.vue
@@ -1,8 +1,66 @@
 <template>
   <div class="container">
-    <b-field>
-      <b-input placeholder="Rechercher un produit ..." size="is-large" icon="search"></b-input>
+    <b-field grouped>
+      <b-input
+        placeholder="Rechercher un produit ..."
+        size="is-large"
+        icon="search"
+        v-model="searchArg"
+        expanded
+      ></b-input>
+      <p class="control">
+        <a class="button is-link is-large" :disabled="searchArg.length === 0">
+          <span>Rechercher</span>
+        </a>
+      </p>
     </b-field>
+    <b-collapse class="card" :open="false">
+      <div slot="trigger" slot-scope="props" class="card-header">
+        <p class="card-header-title">Ajouter des préférences</p>
+        <a class="card-header-icon">
+          <b-icon :icon="props.open ? 'angle-down' : 'angle-up'"></b-icon>
+        </a>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <b-checkbox>Basic</b-checkbox>
+              </div>
+              <div class="field">
+                <b-checkbox>Basic</b-checkbox>
+              </div>
+              <div class="field">
+                <b-checkbox>Basic</b-checkbox>
+              </div>
+            </div>
+            <div class="column">
+              <div class="field">
+                <b-switch>Default</b-switch>
+              </div>
+              <div class="field">
+                <b-switch>Default</b-switch>
+              </div>
+              <div class="field">
+                <b-switch>Default</b-switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-collapse>
   </div>
 </template>
+
+<script>
+export default {
+  name: "ProductHome",
+  data: function() {
+    return {
+      searchArg: ""
+    };
+  }
+};
+</script>
 
