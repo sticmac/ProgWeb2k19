85f8e51fcae4f7db18c92e321f870b2310fb9ca1
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-01-21 16:12:08 +0100

    Separated component for search results

--- client/src/services/router.js
+++ client/src/services/router.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 
 // Views components
 import ProductHome from './../components/Product_Home.vue'
+import ProductSearchResults from './../components/ProductSearchResults.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', component: ProductHome },
+    { path: '/results', component: ProductSearchResults, props: true}
   ]
   
 const router = new VueRouter({
