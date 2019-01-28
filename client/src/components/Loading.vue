084f82377628155dc26ea0a8e5a01d1eff257ba6
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-01-28 15:07:53 +0100

    Loading and placeholder when no results are found

---
+++ client/src/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="loading">
+        <b-icon
+            pack="fas"
+            icon="sync-alt"
+            custom-class="fa-spin">
+        </b-icon>
+        Loading...
+    </div>
+</template>
+
