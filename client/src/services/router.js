30b7616efb71aaa8380bece9651bf661113cb7cb
Unknown <marcjourdes@gmail.com>
2019-01-29 12:33:42 +0100

    Added route for product view

--- client/src/services/router.js
+++ client/src/services/router.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 
 // Views components
 import ProductHome from './../components/Product_Home.vue'
+import ProductView from './../components/ProductView.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', component: ProductHome },
+    { path: '/item/:productId', component: ProductView, props: true}
   ]
   
 const router = new VueRouter({
