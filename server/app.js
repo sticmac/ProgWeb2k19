370366227dfaebe5a50dceeb3b4fd2a3edc703af
Alexandre HILTCHER <alex.hiltcher@gmail.com>
2019-02-14 08:32:51 +0100

    Add allow authorization headers + OPTIONS http verb

--- server/app.js
+++ server/app.js
@@ -21,7 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     next();
 });
 
