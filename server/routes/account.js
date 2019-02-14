c01a571d91ba37e7102f08ab1e2ac22c7e285fc0
Alexandre HILTCHER <alex.hiltcher@gmail.com>
2019-02-14 12:10:11 +0100

    Prevent users from creating several accounts with same email
    prevent

--- server/routes/account.js
+++ server/routes/account.js
@@ -5,16 +5,25 @@ const auth = require("./auth");
 
 
 router.post("/", auth.optional, (req, res, next) => {
-    try {
-        const user = User.build(req.body);
-        user.save();
-        res.status(200);
-        res.send(user.toAuthJSON())
-    } catch (e) {
-        console.error(e);
-        res.status(400);
-        res.send(e)
-    }
+
+    User.get(req.body.email)
+        .then(() => {
+            res.status(400);
+            res.send({error: "Account already exists using email: " + req.body.email})
+        })
+        .catch(() => {
+            try {
+                const user = User.build(req.body);
+                user.save();
+                res.status(200);
+                res.send(user.toAuthJSON())
+            } catch (e) {
+                console.error(e);
+                res.status(400);
+                res.send(e)
+            }
+        });
+
 });
 
 module.exports = router;
